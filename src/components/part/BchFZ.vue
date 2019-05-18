9eecdd3cf8f195529786ee1e6cadbe79a28f0ad9
jobs <1620252479@qq.com>
2019-05-18 10:40:10 +0800

    病虫害防治-

--- src/components/part/BchFZ.vue
+++ src/components/part/BchFZ.vue
@@ -1,45 +1,41 @@
 <template>
   <transition enter-active-class="animated bounceIn" leave-active-class="animated bounceOut">
-   <Card>
-     <Tabs value="name1">
-       <TabPane label="草坪" name="name1">
-         <Card style="width:350px">
-           <!--          分类-->
-           <Select v-model="type" style="width:200px">
-             <Option v-for="item in bchType" :value="item.value" :key="item.value">{{ item.label }}</Option>
-           </Select>
-           <!--          常见病虫害-->
-           <Collapse v-model="bchItem">
-             <Panel name="1" v-for="item in bchList">
-               {{item.name}}
-               <p slot="content">{{item.content}}</p>
-             </Panel>
-           </Collapse>
+      <Card style="width:350px">
+          <!--           植被类型选择-->
+          <Select v-model="zbType" style="width:200px" @change="changeZb">
+              <Option v-for="item in zbTypeList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
 
-         </Card>
-       </TabPane>
-       <TabPane label="树木" name="name2">
-         <Card style="width:350px">
-<!--           树木类型选择-->
-           <Select v-model="type" style="width:200px">
-             <Option v-for="item in bchType" :value="item.value" :key="item.value">{{ item.label }}</Option>
-           </Select>
-<!--树木列表-->
-           <Select v-model="type" style="width:200px">
-             <Option v-for="item in bchType" :value="item.value" :key="item.value">{{ item.label }}</Option>
-           </Select>
+          <!--选中树木：显示树木列表-->
+          <Select v-model="treeType" style="width:200px" @change="changeTreeType">
+              <Option v-for="item in treeTypeList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
 
-           
+          <!--选中树木：树木类型-->
+          <Select v-model="tree" style="width:200px" @change="changeTree">
+              <Option v-for="item in treeList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
 
-         </Card>
-       </TabPane>
-     </Tabs>
-     <!--    趋势图-->
-     <Card :bordered="false">
-       <p slot="title">病虫害趋势图</p>
-       <p id="chart"></p>
-     </Card>
-   </Card>
+
+          <!--     病虫害 分类-->
+          <Select v-model="bchType" style="width:200px" @change="changeBchType">
+              <Option v-for="item in bchTypeList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+
+          <!--  常见病虫害-->
+          <Collapse v-model="bchItem">
+              <Panel name="1" v-for="item in bchList">
+                  {{item.name}}
+                  <p slot="content">{{item.content}}</p>
+              </Panel>
+          </Collapse>
+          <!--    趋势图-->
+          <Card :bordered="false">
+              <p slot="title">病虫害趋势图</p>
+              <p id="chart"></p>
+          </Card>
+
+      </Card>
   </transition>
 </template>
 
@@ -47,14 +43,23 @@
     export default {
         name: "BchFZ",
         props:{
-          bchList:Array,
-          bchType:Array,
+            bch:Object,
         },
         data(){
           return{
               visible:false,
+              bchList:[],
+              bchType:{},
+              bchTypeList:[],
               type:{},
-             bchItem:{}
+              zbType:{},
+              zbTypeList:this.bch.zbTypeList,
+              treeType:{},
+              treeTypeList:[],
+              tree:{},
+              treeList:[],
+              bchItem:{},
+
           }
         },
 
@@ -65,12 +70,29 @@
             fun.moveElement(this.$el)
         },
       methods:{
+          changeZb(){
+            if (this.zbType.name.i){
+
+            }
+          },
+          changeTreeType(){
+
+          },
+          changeTree(){
+
+          },
+          changeBchType(){
+
+          },
 
       },
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+#chart{
+    width: 100%;
+    height: 300px;
+}
 
 </style>
